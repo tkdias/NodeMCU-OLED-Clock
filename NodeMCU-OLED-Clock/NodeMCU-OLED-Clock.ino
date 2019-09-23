0e3d38284b7c74004879810f9a93cf4fa62338f3
Tharindu Dias <tkdias0@gmail.com>
2019-09-23 23:06:39 +0530

    Code loop section

--- NodeMCU-OLED-Clock/NodeMCU-OLED-Clock.ino
+++ NodeMCU-OLED-Clock/NodeMCU-OLED-Clock.ino
@@ -72,4 +72,53 @@ void setup()
 void loop()
 {
 
+time_t now = time(nullptr);
+  struct tm* p_tm = localtime(&now);
+
+  Serial.print(p_tm->tm_mday);
+  Serial.print("/");
+  Serial.print(p_tm->tm_mon + 1);
+  Serial.print("/");
+  Serial.print(p_tm->tm_year + 1900);
+
+  Serial.print(" ");
+
+  Serial.print(p_tm->tm_hour);
+  Serial.print(":");
+  Serial.print(p_tm->tm_min);
+  Serial.print(":");
+  Serial.println(p_tm->tm_sec);
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+
+  display.setCursor(0, 0);
+  display.print(p_tm->tm_hour);
+  display.print(":");
+  if ( p_tm->tm_min < 10)
+    display.print("0");
+  display.print(p_tm->tm_min);
+
+  display.setTextSize(2);
+  display.setCursor(90, 5);
+  display.print(".");
+  if ( p_tm->tm_sec < 10)
+    display.print("0");
+  display.print(p_tm->tm_sec);
+
+  display.setTextSize(1);
+  display.setCursor(0, 40);
+  display.print(p_tm->tm_mday);
+  display.print("/");
+  display.print(p_tm->tm_mon + 1);
+  display.print("/");
+  display.print(p_tm->tm_year + 1900);
+
+  display.display();
+
+  delay(1000); // update every 1 sec
+  
 }
